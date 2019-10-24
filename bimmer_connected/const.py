5887b567dd50fb1c7f2edc34a5c159bda5f3f863
robthebold <38596885+robthebold@users.noreply.github.com>
2019-10-23 20:39:17 -0500

    bimmer_connected library mods
    modifications to the bimmer_connected library to provide additional experimental features

---
+++ bimmer_connected/const.py
@@ -0,0 +1,14 @@
+"""urls for different services."""
+
+AUTH_URL = 'https://{server}/gcdm/oauth/token'
+BASE_URL = 'https://{server}/webapi/v1'
+
+VEHICLES_URL = BASE_URL + '/user/vehicles'
+VEHICLE_VIN_URL = VEHICLES_URL + '/{vin}'
+VEHICLE_STATUS_URL = VEHICLE_VIN_URL + '/status'
+REMOTE_SERVICE_STATUS_URL = VEHICLE_VIN_URL + '/serviceExecutionStatus?serviceType={service_type}'
+REMOTE_SERVICE_URL = VEHICLE_VIN_URL + "/executeService"
+VEHICLE_IMAGE_URL = VEHICLE_VIN_URL + "/image?width={width}&height={height}&view={view}"
+VEHICLE_POI_URL = VEHICLE_VIN_URL + '/sendpoi'
+VEHICLE_STATISTICS_URL = VEHICLE_VIN_URL + '/statistics'
+VEHICLE_API = 'https://{server}/api/vehicle'
